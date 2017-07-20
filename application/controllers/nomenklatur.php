06b489d12a22418babdba496691bcae82765130f
abdulhadikaryana <karyana.abdhadi@gmail.com>
2017-07-20 15:24:03 +0700

    perbaikan controller Nomenklatur dan form form_nomenklatur. add fungsi form_prosess di controller Nomenklatur

--- application/controllers/nomenklatur.php
+++ application/controllers/nomenklatur.php
@@ -1,18 +1,23 @@
 
 <?php
-class nomenklatur extends CI_Controller
+class Nomenklatur extends CI_Controller
 {
 	function _Construct()
 	{
 		parent ::_Construct();
 	}
 	
-	function simpan_dinas()
+	function index()
 	{
 		$this->load->model('dinas_model');
-		$this->dinas_model->simpan_dinas();
+		//$this->dinas_model->simpan_dinas();
 		$this->load->view('form_nomenklatur');
 
+	}
+
+	function form_proses(){
+		$data_dinas['nama_dinas']= $this->input->post('nama_dinas');
+		$dasar_hukum=$this->input->post('dasar_hukum');
 	}
 	
 }
